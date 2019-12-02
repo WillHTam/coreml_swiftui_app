66a75c6d83ddc044ba9ecd9020a1fe4b9d4b8adc
William T <william.tam18@gmail.com>
2019-12-03 02:33:54 +0800

    trained model

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,6 +2,22 @@
 import CreateML
 import Foundation
 
-let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/William/Dev/Projects/Swift/SleepApp/bettter-rest.json"))
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/William/Dev/Projects/Swift/SleepApp/better-rest.json"))
 
+// test train split
+let (trainingData, testingData) = data.randomSplit(by: 0.8)
+
+// Set input and target column from JSON
+let regressor = try MLRegressor(trainingData: trainingData, targetColumn: "actualSleep")
+
+let evaluationMetrics = regressor.evaluation(on: testingData)
+print("<<<RMSE>>>", evaluationMetrics.rootMeanSquaredError)
+print(evaluationMetrics.maximumError)
+print("<<<model>>>", regressor.model)
+print("<<<description>>", regressor.description)
+print("<<<debug description>>>", regressor.debugDescription)
+
+let metadata = MLModelMetadata(author: "William", shortDescription: "A model trained to predict optimum sleep times for coffee drinkers.", version: "1.0")
+
+try regressor.write(to: URL(fileURLWithPath: "/Users/William/Dev/Projects/Swift/SleepApp/SleepModel.mlmodel"), metadata: metadata)
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Sleep CoreML App
+
+https://developer.apple.com/documentation/createml/mlregressor
+mlmodel uses LinReg, was automatically chosen by MLRegressor
+see regressor.description after running .evaluate 
