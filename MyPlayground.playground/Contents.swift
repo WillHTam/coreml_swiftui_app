64f93422436c8e3faf718665f34e2edd9709dd4d
William T <william.tam18@gmail.com>
2019-12-03 02:21:08 +0800

    init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+// This is a MacOS Playground
+import CreateML
+import Foundation
+
+let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/William/Dev/Projects/Swift/SleepApp/bettter-rest.json"))
+
+
